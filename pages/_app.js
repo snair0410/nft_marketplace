363af7a20ac34164d45a887d38c6a8e03a342883
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-23 08:04:48 +0530

    setting up reusable components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/f346e7e2c6.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;
